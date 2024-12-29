1898630dd1be2443c3bfc92cd616be959ae87129
Jorge Solis <solisjorge1302@gmail.com>
2024-12-28 21:15:44 -0600

    routing
    Adding routes for admin and public  components

--- tec-mi-ui/src/app/app-routing.module.ts
+++ tec-mi-ui/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin/admin.component';
+import { ProductComponent } from './products/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: 'admin', component:AdminComponent},
+  {path: 'products', component:ProductComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
