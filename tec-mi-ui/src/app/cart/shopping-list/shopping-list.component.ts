8af0161ca8e17566ece8899b866ff58e69e2480e
Jorge Solis <solisjorge1302@gmail.com>
2025-01-05 00:03:08 -0600

    shopping list details
    added item list and badge display fro the shopping  cart

--- tec-mi-ui/src/app/cart/shopping-list/shopping-list.component.ts
+++ tec-mi-ui/src/app/cart/shopping-list/shopping-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/products/product.model';
+import { Item } from 'src/app/shared/item';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+
+  cartItems:Item[]=[
+    new Item(new  Product(3,"Playera3","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",99,"003"),3,"S"),
+    new Item(new  Product(3,"Playera3","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",99,"003"),2,"M")
+  ];
 
 }
