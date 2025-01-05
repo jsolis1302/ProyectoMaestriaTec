8043e9235269d96bbed489c5e11ac352b5230003
Jorge Solis <solisjorge1302@gmail.com>
2025-01-05 00:11:37 -0600

    shopping list logic
    added logic to calculate total price based on qty and product price

--- tec-mi-ui/src/app/cart/shopping-list/shopping-list.component.ts
+++ tec-mi-ui/src/app/cart/shopping-list/shopping-list.component.ts
@@ -10,8 +10,10 @@ import { Item } from 'src/app/shared/item';
 export class ShoppingListComponent {
 
   cartItems:Item[]=[
-    new Item(new  Product(3,"Playera3","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",99,"003"),3,"S"),
-    new Item(new  Product(3,"Playera3","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",99,"003"),2,"M")
+    new Item(new  Product(3,"Playera3","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",100,"003"),3,"S"),
+    new Item(new  Product(3,"Playera3","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",100,"003"),2,"M")
   ];
+
+   total = this.cartItems.reduce((sum, current) => sum + (current.qty * current.product.price), 0);
 
 }
