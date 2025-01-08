2ee28568c735309c9711dc495ae0fd1a8cbe1459
Jorge Solis <solisjorge1302@gmail.com>
2025-01-08 16:03:34 -0600

    component changes
    Shopping cart view completed and removed un used code in header component

--- tec-mi-ui/src/app/cart/shopping-list/shopping-list.component.ts
+++ tec-mi-ui/src/app/cart/shopping-list/shopping-list.component.ts
@@ -11,7 +11,9 @@ export class ShoppingListComponent {
 
   cartItems:Item[]=[
     new Item(new  Product(3,"Playera3","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",100,"003"),3,"S"),
-    new Item(new  Product(3,"Playera3","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",100,"003"),2,"M")
+    new Item(new  Product(3,"Playera3","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",100,"003"),2,"M"),
+    new Item(new  Product(3,"Playera3","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",100,"003"),4,"L")
+
   ];
 
    total = this.cartItems.reduce((sum, current) => sum + (current.qty * current.product.price), 0);
