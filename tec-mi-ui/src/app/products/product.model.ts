5c701bea8a436b2886165098c9819ad8d9ba22ab
Jorge Solis <solisjorge1302@gmail.com>
2025-01-09 19:21:32 -0600

    product model
    added active property to the product model

--- tec-mi-ui/src/app/products/product.model.ts
+++ tec-mi-ui/src/app/products/product.model.ts
@@ -1,6 +1,14 @@
 export class Product{
 
 
-    constructor(public productId:number, public name:string, public description:string, public imagePath:string, public price:number,public code:string ){}
+    constructor(
+        public productId:number, 
+        public name:string, 
+        public description:string, 
+        public imagePath:string, 
+        public price:number,
+        public code:string,
+        public active:boolean
+     ){}
 }
 
--- tec-mi-ui/src/app/shared/shared.service.ts
+++ tec-mi-ui/src/app/shared/shared.service.ts
@@ -9,21 +9,21 @@ import { Client } from './client.model';
 export class SharedService {
 
   cartItems:Item[]=[
-    new Item(new  Product(3,"Playera3","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",100,"003"),3,"S"),
-    new Item(new  Product(3,"Playera3","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",100,"003"),2,"M"),
-    new Item(new  Product(3,"Playera3","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",100,"003"),4,"L")
+    new Item(new  Product(3,"Playera3","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",100,"003",true),3,"S"),
+    new Item(new  Product(3,"Playera3","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",100,"003",true),2,"M"),
+    new Item(new  Product(3,"Playera3","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",100,"003",true),4,"L")
 
   ];
 
   products:Product[] = [
-    new  Product(1,"Playera","Playera Nueva ideaal para el trabajo o una salida casual, 100% algodon","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12.jpg",120,"001"),
-    new  Product(2,"Playera2","Playera Nueva ideaal para el trabajo o una salida casual, 100% algodon","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13.jpg",139,"002"),
-    new  Product(3,"Playera3","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",99,"003"),
-    new  Product(4,"Playera4","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",179,"001"),
-    new  Product(5,"Playera5","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13.jpg",139,"001"),
-    new  Product(6,"Playera6","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14.jpg",99,"001"),
-    new  Product(7,"Playera7","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",199,"001"),
-    new  Product(8,"Playera8","Playera Nueva ideaal para el trabajo o una salida casual, 100% algodon","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12.jpg" ,120,"001")
+    new  Product(1,"Playera","Playera Nueva ideaal para el trabajo o una salida casual, 100% algodon","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12.jpg",120,"001",true),
+    new  Product(2,"Playera2","Playera Nueva ideaal para el trabajo o una salida casual, 100% algodon","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13.jpg",139,"002",true),
+    new  Product(3,"Playera3","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",99,"003",true),
+    new  Product(4,"Playera4","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",179,"001",true),
+    new  Product(5,"Playera5","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13.jpg",139,"001",true),
+    new  Product(6,"Playera6","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14.jpg",99,"001",true),
+    new  Product(7,"Playera7","Playera Nueva","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",199,"001",true),
+    new  Product(8,"Playera8","Playera Nueva ideaal para el trabajo o una salida casual, 100% algodon","https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12.jpg" ,120,"001",true)
   ]
 
   user:Client = new Client("Jorge","Jorge.solis@mail.com",552211447,1,"A");
