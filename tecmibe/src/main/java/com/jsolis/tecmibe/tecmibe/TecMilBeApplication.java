232934a1cc878f2e6185751ffb1ab0e5626da603
Jorge Solis <solisjorge1302@gmail.com>
2025-01-14 14:10:27 -0600

    Backend starter project
    Added Backend starter project

---
+++ tecmibe/src/main/java/com/jsolis/tecmibe/tecmibe/TecMilBeApplication.java
@@ -0,0 +1,13 @@
+package com.jsolis.tecmibe.tecmibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecMilBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecMilBeApplication.class, args);
+	}
+
+}
---
+++ tecmibe/src/test/java/com/jsolis/tecmibe/tecmibe/TecMilBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsolis.tecmibe.tecmibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecMilBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
