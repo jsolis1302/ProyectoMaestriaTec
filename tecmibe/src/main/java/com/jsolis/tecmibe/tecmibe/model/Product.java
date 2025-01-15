97d58475f236e19e0282d9593f665d445c7eca08
Jorge Solis <solisjorge1302@gmail.com>
2025-01-14 18:57:56 -0600

    Product model attributes privacy
    Changed attributes from public to private in Product model

--- tecmibe/src/main/java/com/jsolis/tecmibe/tecmibe/model/Product.java
+++ tecmibe/src/main/java/com/jsolis/tecmibe/tecmibe/model/Product.java
@@ -11,11 +11,11 @@ import lombok.Data;
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public int productId;
-    public String name;
-    public String description;
-    public String imagePath;
-    public double price;
-    public String code;
-    public boolean active;
+    private int productId;
+    private String name;
+    private String description;
+    private String imagePath;
+    private double price;
+    private String code;
+    private boolean active;
 }
