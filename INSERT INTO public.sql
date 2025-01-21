5aca46e5d427408f4f92ad38d7654814b90d3291
Jorge Solis <solisjorge1302@gmail.com>
2025-01-20 19:41:43 -0600

    Update INSERT INTO public.sql
    added queries and api  list

--- INSERT INTO public.sql
+++ INSERT INTO public.sql
@@ -9,11 +9,11 @@ INSERT INTO public.products(
 	'Playera 3', 86
 	);
 
-INSERT INTO public.address(
+INSERT INTO public.addresses(
 	address_id, city, country, name, numb, postal_code, state, street, client_id)
 	VALUES (1, 'Merida', 'Mexico', 'casa', '519', '97302', 'Yuc', '53-a', 1);
 
-INSERT INTO public.address(
+INSERT INTO public.addresses(
 	address_id, city, country, name, numb, postal_code, state, street, client_id)
 	VALUES (2, 'Merida', 'Mexico', 'Oficina', '874', '97300', 'Yuc', '53-b', 1);
 
@@ -28,5 +28,28 @@ INSERT INTO public.clients(
 	VALUES (2, 'admin', 'jorge_solis@hotmail.com', 'Raul Solis', 9999992413);
 
 
-http://localhost:8080/products/allActiveProducts
-http://localhost:8080/clients/allClients
+select * from addresses
+
+delete from address where address_id = 2 commit;
+
+select * from clients;
+
+insert into clients (id, active, client_type, email, name,phone) 
+	select client_id, active, client_type, email, name,phone from client where client_id = 2;
+
+
+drop table clients;
+
+drop table address;
+
+select * from products;
+
+insert into products (active, price,id,code,description,image_path,name)
+select active, price,product_id,code,description,image_path,name from product;
+
+
+http://localhost:8080/api/products/product
+http://localhost:8080/api/clients/2/addresses
+http://localhost:8080/api/products/allActiveProducts
+http://localhost:8080/api/clients/allClients
+http://localhost:8080/api/clients/allActiveClients
